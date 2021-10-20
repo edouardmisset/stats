287f62bc6d5b100a33f45f87340dced7ed253994
Edouard Misset <edouardmisset@gmail.com>
2021-10-20 15:39:39 +0200

    Created a MatchData type (a tuple of date, strings, numbers...)

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,37 @@
 import fs from 'fs'
+import { MatchResult } from './MatchResults'
+import { dateStringToDate } from './utils'
+
+export type MatchData = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+]
 
 export class CsvFileReader {
-  data: string[][] = []
+  data: MatchData[] = []
+
   constructor(public filename: string) {}
+
   read(): void {
     this.data = fs
       .readFileSync(this.filename, { encoding: 'utf-8' })
       .split('\n')
       .map((match: string): string[] => match.split(','))
+      .map(
+        (match: string[]): MatchData => [
+          dateStringToDate(match[0]),
+          match[1],
+          match[2],
+          parseInt(match[3]),
+          parseInt(match[4]),
+          match[5] as MatchResult,
+          match[6],
+        ]
+      )
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-import { CsvFileReader } from './CsvFileReader'
+import { CsvFileReader, MatchData } from './CsvFileReader'
+import { MatchResult } from './MatchResults'
 
 const reader = new CsvFileReader('football.csv')
 reader.read()
 
-// enum - enumeration
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
-
 let manUnitedWins = 0
 
-reader.data.forEach((match: string[]): void => {
+reader.data.forEach((match: MatchData): void => {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
